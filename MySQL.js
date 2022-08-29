b95d2c21b5c42098f1b4b095835c52a7c2445ec8
laurelstrelzoff <57377421+laurelstrelzoff@users.noreply.github.com>
2022-08-28 19:43:58 -0500

    Added print_table function
    Added a function to print a table

--- MySQL.js
+++ MySQL.js
@@ -2,12 +2,16 @@ var mysql = require('mysql');
 
 var username = "defaultUser";
 var password = "defaultPass";
+var database = "defaultDatabase";
+var table = "defaultTable"
+
 connection = null;
 
-if (connection == null){ connection = createConnection(username, password); } else return;
+if (connection == null){ connection = createConnection(username, password, database); } else return;
 
 connect(connection);
 disconnect(connection);
+// print_table(connection, table);
 
 function createConnection(username, password)
 { 
@@ -32,4 +36,13 @@ function disconnect(con)
     con.on('end', function() {
         console.log("Disconnected from MySQL Server");
     })
+}
+
+function print_table(con, table)
+{
+    query = "SELECT * FROM " + table;
+    con.query(query, function (err, result, fields) {
+        if (err) throw err;
+        console.log(result);
+      });
 }
