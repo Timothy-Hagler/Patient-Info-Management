1e4e0bf00ca87aaaa7ad918bc3daad3282ac00bc
Timothy-Hagler <tmhglr@gmail.com>
2022-10-29 13:51:24 -0500

    remove node modules copy

--- patient_information_management/installer.sh
+++ patient_information_management/installer.sh
@@ -2,5 +2,4 @@
 
 cp patient_information_management/package.json .
 cp patient_information_management/package-lock.json .
-cp -r patient_information_management/node_modules .
 cat requirements.txt | npm install -g --save-dev
