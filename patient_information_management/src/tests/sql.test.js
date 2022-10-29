c3360643d7d569f509d02905138a26e3ae6a9937
Timothy-Hagler <tmhglr@gmail.com>
2022-10-29 14:24:43 -0500

    fix mysql file

--- patient_information_management/src/tests/sql.test.js
+++ patient_information_management/src/tests/sql.test.js
@@ -3,6 +3,7 @@ const sql = require('../../MySQL')
 test("Create valid connection to server",() => {
     let connection = sql.createConnection("test_user", "passwordtest", "anotherTest")
     expect(connection).not.toBeNull()
+    sql.disconnect(connection)
 })
 
 test("Connect to Server Successfully",() => {
@@ -10,6 +11,7 @@ test("Connect to Server Successfully",() => {
     expect(() => {
         sql.connect(connection)
     }).not.toThrow()
+    sql.disconnect(connection)
 })
 
 test("Disconnect from Server Successfully",() => {
