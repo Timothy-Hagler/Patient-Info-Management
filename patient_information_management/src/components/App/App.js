96c25710e5a58024a86770fef5ba30d942f49ce7
Timothy-Hagler <tmhglr@gmail.com>
2022-10-29 13:27:24 -0500

    feat(nav bar): add navigation bar

--- patient_information_management/src/components/App/App.js
+++ patient_information_management/src/components/App/App.js
@@ -4,12 +4,30 @@ import React, {useState} from 'react';
 import Patient_List from '../Patient_List/Patient_List';
 import Login from '../Login/Login';
 import Patient_Information from '../Patient_Information/Patient_Information';
+import logo from '../images/PIMS_emblem.png';
 
 
 function App() {
   return(
     <div className = "app">
         <h1>Patient Information Management System</h1>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <a class="navbar-brand" href="/"><img src={logo} alt="logo" width="32" height="32"></img></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                  <a class="nav-link" href="/patient-information">Patient Information<span class="sr-only"></span></a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="/patient-list">Patient List</a>
+                </li>
+              </ul>
+            </div>
+          </nav>
         <BrowserRouter>
           <Routes>   
             <Route path="/" element={<Login />} />
