787c0f3388f7321e8d9f3c3e146e0fb8cbf53d1f
laurelstrelzoff <57377421+laurelstrelzoff@users.noreply.github.com>
2022-11-06 16:20:49 -0600

    Renders each of the pages before every test

--- patient_information_management/src/components/Login/Login.test.js
+++ patient_information_management/src/components/Login/Login.test.js
@@ -1,3 +1,17 @@
-test("Dummy About Us Test",() => {
+import { render, screen } from '@testing-library/react';
+import {MemoryRouter} from 'react-router-dom';
+import Login from './Login.js';
 
+beforeAll(() => {
+    render(
+        <MemoryRouter>
+            <Login/>
+        </MemoryRouter>);
+})
+
+test("Dummy Login Test",() => {
+    const header = screen.getByText('Login');
+    const note = screen.getByText('Credentials');
+    expect(header).toBeInTheDocument();
+    expect(note).toBeInTheDocument();
 })
--- patient_information_management/src/components/Patient_List/Patient_List.test.js
+++ patient_information_management/src/components/Patient_List/Patient_List.test.js
@@ -1,3 +1,14 @@
-test("Dummy About Us Test",() => {
+import { render, screen } from '@testing-library/react';
+import {MemoryRouter} from 'react-router-dom';
+import Patient_List from './Patient_List.js';
+
+beforeAll(() => {
+    render(
+        <MemoryRouter>
+            <Patient_List/>
+        </MemoryRouter>);
+})
+
+test("Dummy Patient List Test",() => {
 
 })
