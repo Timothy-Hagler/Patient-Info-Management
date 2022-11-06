419c8c857180793fcc21f11dd1f1f5ff015afb1b
laurelstrelzoff <57377421+laurelstrelzoff@users.noreply.github.com>
2022-11-06 16:39:54 -0600

    Added some guidelines for testing

--- patient_information_management/src/components/Login/Login.test.js
+++ patient_information_management/src/components/Login/Login.test.js
@@ -9,9 +9,16 @@ beforeAll(() => {
         </MemoryRouter>);
 })
 
-test("Dummy Login Test",() => {
+// tests if the login page is appearing correctly
+test("Login page rendering test",() => {
     const header = screen.getByText('Login');
     const note = screen.getByText('Credentials');
     expect(header).toBeInTheDocument();
     expect(note).toBeInTheDocument();
 })
+
+test("Login Page API Tests", () => { 
+    // test that the login button connects to the correct api call
+    // do not test whether the login api call is accurate, that is done in sql.test.js
+});
+
--- patient_information_management/src/components/Patient_List/Patient_List.test.js
+++ patient_information_management/src/components/Patient_List/Patient_List.test.js
@@ -9,6 +9,29 @@ beforeAll(() => {
         </MemoryRouter>);
 })
 
-test("Dummy Patient List Test",() => {
 
-})
+
+describe('Patient List Rendering Tests', () => { 
+    // test if we are on the patient page by checking for unique text
+    test("Patient List Rendering Test",() => {
+        
+    })
+
+    // test the appearance of the modal and its dialogues (the extra ones for save and delete patient)
+    // test that modal appears when it should (when clicking on a person)
+    test("Patient List Modal Test",() => {
+
+        
+    })
+});
+
+
+describe('Patient List API Tests', () => { 
+    // test that each button in the regular page (not in modal) makes the correct call to the API
+    // doesn't test the functionality of the api calls, that will happen in sql.test.js
+});
+
+describe('Modal Dialogue API Tests', () => { 
+    // test appearance of modal (buttons, forms, etc). test this in each route (delete patient, save patient extra dialogues)
+    // test each of the modal buttons to see if they connect to the correct api calls, don't test functionality of api calls here
+});
