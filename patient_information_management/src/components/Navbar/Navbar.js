ee15b718f7f608db54572ef30fe011b3d8d24d8b
laurelstrelzoff <57377421+laurelstrelzoff@users.noreply.github.com>
2022-11-16 19:04:26 -0600

    Updated navbar from main

--- patient_information_management/src/components/Navbar/Navbar.js
+++ patient_information_management/src/components/Navbar/Navbar.js
@@ -2,25 +2,35 @@ import logo from '../images/PIMS_emblem.png';
 
 export default function Navbar(){
     return(
-        <nav className="navbar navbar-expand-lg" data-testid='navBar'>
-        <a className="navbar-brand" href="/" data-testid='logo'><img src={logo} alt="logo" width="32" height="32"></img></a>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
+      <nav class="navbar navbar-expand-lg">
+      <img src={logo} alt="logo" width="32" height="32"></img>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
 
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav mr-auto">
-            <li className="nav-item">
-              <a className="nav-link"  data-testid = 'patientList' href="/patient-list">Patient List</a>
-            </li>
-            <li className="nav-item" data-testid = 'aboutUs'>
-              <a className="nav-link"  href="/about-us">About Us</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link"  data-testid = 'helpPage' href="/help-page">Help</a>
-            </li>
-          </ul>
-        </div>
-     </nav>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar navbar-nav mr-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="/patient-list">Patient List</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/about-us">About Us</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/help-page">Help</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/patient-search">Search</a>
+          </li>
+        </ul>
+      </div>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar navbar-nav ml-auto">
+          <li class="nav-item" id="logOutButton">
+            <a class="nav-link" href="/">Log Out</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
     );
 }
