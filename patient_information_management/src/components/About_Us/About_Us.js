70aa461535ad1cdbf3a06f1a38b492dd8c6b2c89
Timothy-Hagler <tmhglr@gmail.com>
2022-12-02 22:07:57 -0600

    format some more

--- patient_information_management/src/components/About_Us/About_Us.js
+++ patient_information_management/src/components/About_Us/About_Us.js
@@ -7,53 +7,71 @@ Fall 2022
 */
 
 //import libraries
-import React from 'react';
-import Card from 'react-bootstrap/cjs/Card.js';
-import './About_Us.css';
-
+import React from "react";
+import Card from "react-bootstrap/cjs/Card.js";
+import "./About_Us.css";
 
 const About_Us = () => {
   return (
     <section>
-    <div class="container">
-        <h2><b>About Us</b></h2>
+      <div class="container">
+        <h2>
+          <b>About Us</b>
+        </h2>
         <br></br>
         <hr />
         <br></br>
         <div class="row">
-        <div class="col-sm">
-          <Card border="primary" className="PIMSCard">
-          <Card.Header id="cardHeader"><h4>PIMS</h4></Card.Header>
-          <Card.Body>
-            {/*description of the PIMS system */}
-          <p>
-             The Patient Information Management System, also known as PIMS, will allow for doctors, nurses, office staff,
-             and volunteers to access different levels of patient data based on their job. Doctors can access all patient
-             information and write additional notes on each patient. Nurses can access the same information but can only
-             view the doctors' notes. Office staff will be able to access and update information about patients such as 
-             name, address, and insurance information. Volunteers will be able to help office staff.
-          </p>
-          </Card.Body>
-          </Card>
+          <div class="col-sm">
+            <Card border="primary" className="PIMSCard">
+              <Card.Header id="cardHeader">
+                <h4>PIMS</h4>
+              </Card.Header>
+              <Card.Body>
+                {/*description of the PIMS system */}
+                <p>
+                  The Patient Information Management System, also known as PIMS,
+                  will allow for doctors, nurses, office staff, and volunteers
+                  to access different levels of patient data based on their job.
+                  Doctors can access all patient information and write
+                  additional notes on each patient. Nurses can access the same
+                  information but can only view the doctors' notes. Office staff
+                  will be able to access and update information about patients
+                  such as name, address, and insurance information. Volunteers
+                  will be able to help office staff.
+                </p>
+              </Card.Body>
+            </Card>
+          </div>
+          <div class="col-sm">
+            <Card border="primary" className="AdminCard">
+              <Card.Header id="cardHeader">
+                <h4>Administrators</h4>
+              </Card.Header>
+              <Card.Body>
+                {/*show the contact information of the system administrators in case of errors or questions */}
+                <p>
+                  The administrators of this project will need to be contacted
+                  in order to create a new user account. If any other issues
+                  arise, do not hesitate to contact the administrators.
+                </p>
+                <p>
+                  <b>Benjamin Stone</b> <center>bas0043@uah.edu</center>
+                </p>
+                <p>
+                  <b>Sydney Keller</b> <center>smk0023@uah.edu</center>
+                </p>
+                <p>
+                  <b>Laurel Strelzoff</b> <center>lcs0018@uah.edu</center>
+                </p>
+                <p>
+                  <b>Timothy Hagler</b> <center>trh0030@uah.edu</center>
+                </p>
+              </Card.Body>
+            </Card>
+          </div>
         </div>
-        <div class="col-sm">
-          <Card border="primary" className="AdminCard">
-          <Card.Header id="cardHeader"><h4>Administrators</h4></Card.Header>
-          <Card.Body>
-          {/*show the contact information of the system administrators in case of errors or questions */}
-          <p>
-            The administrators of this project will need to be contacted in order to create a new user account. If any
-            other issues arise, do not hesitate to contact the administrators.
-          </p>
-          <p><b>Benjamin Stone</b>  <center>bas0043@uah.edu</center></p>
-          <p><b>Sydney Keller</b>   <center>smk0023@uah.edu</center></p>
-          <p><b>Laurel Strelzoff</b> <center>lcs0018@uah.edu</center></p>
-          <p><b>Timothy Hagler</b>    <center>trh0030@uah.edu</center></p>
-          </Card.Body>
-          </Card>
-        </div>
-        </div>
-    </div>
+      </div>
     </section>
   );
 };
--- patient_information_management/src/components/Login/Login.js
+++ patient_information_management/src/components/Login/Login.js
@@ -1,69 +1,58 @@
-import React, { Component, useState } from 'react';
-import './Login.css';
+import React, { Component, useState } from "react";
+import "./Login.css";
 
-import Button from 'react-bootstrap/cjs/Button.js';
-import Card from 'react-bootstrap/cjs/Card.js';
-import PropTypes from 'prop-types';
+import Button from "react-bootstrap/cjs/Button.js";
+import Card from "react-bootstrap/cjs/Card.js";
+import PropTypes from "prop-types";
 import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
-import Axios from 'axios';
-import Alert from 'react-bootstrap/cjs/Alert.js';
+import Axios from "axios";
+import Alert from "react-bootstrap/cjs/Alert.js";
 
-var accountType = sessionStorage.getItem("accountType")
-
+var accountType = sessionStorage.getItem("accountType");
 
 //async function required for login-authentication
 //to 8080 port where authentication server runs
 async function loginUser(credentials) {
+  return fetch("http://localhost:8080/login", {
+    method: "POST",
 
-  return fetch('http://localhost:8080/login', {
- 
-    method: 'POST',
- 
     headers: {
- 
-      'Content-Type': 'application/json'
- 
+      "Content-Type": "application/json",
     },
- 
-    body: JSON.stringify(credentials)
- 
-  })
- 
-    .then(data => data.json())
- 
- }
+
+    body: JSON.stringify(credentials),
+  }).then((data) => data.json());
+}
 
 export default function Login({ setToken }) {
-  
   //Navigation functions
   const navigate = useNavigate();
 
   const navigateToHelp = () => {
-    navigate('/help-page');
-  }
+    navigate("/help-page");
+  };
 
   const navigateToLogin = () => {
-    navigate('/');
-  }
+    navigate("/");
+  };
 
   const navigateToPatientList = () => {
-    navigate('/patient-list');
-  }
+    navigate("/patient-list");
+  };
 
   const navigateToAboutUs = () => {
-    navigate('/about-us');
-  }
+    navigate("/about-us");
+  };
 
-  const [username, setUserName] = useState(); 
-  
+  const [username, setUserName] = useState();
+
   const [password, setPassword] = useState();
 
   const [show, setShow] = useState(false); //Used for alert
 
-  function incorrectUsernameOrPassword()
-  {
-    if(JSON.parse(sessionStorage.getItem("showMessage")))
-      return(
+  function incorrectUsernameOrPassword() {
+    if (JSON.parse(sessionStorage.getItem("showMessage")))
+      return (
         <>
           <div>
             <Alert variant="danger" onClose={() => setShow(false)}>
@@ -71,91 +60,104 @@ export default function Login({ setToken }) {
             </Alert>
           </div>
         </>
-      )
-      
+      );
   }
 
   function LoadUsernameAndPassword(selection, schema, table, location, data) {
-    let url = (`http://localhost:8080/api/getUsernameAndPassword/?selection=${selection}&schema=${schema}&table=${table}&location=${location}&data=${data}`)
-    
-    Axios.get(url).then((response)=>{
-        // this will insert the data of the patient
-        let data = response.data[0]
+    let url = `http://localhost:8080/api/getUsernameAndPassword/?selection=${selection}&schema=${schema}&table=${table}&location=${location}&data=${data}`;
 
-        //If username is incorrect
-        if (data == null)
-        {
-            sessionStorage.setItem("showMessage", JSON.stringify(true));
-            window.location.reload();
-        }
-        
-        //If password is incorrect
-        if(password === data["password"])
-        {
-          sessionStorage.setItem("isLoggedIn", JSON.stringify(true))
-          sessionStorage.setItem("accountType", data["type"])
-          sessionStorage.setItem("showMessage", JSON.stringify(false))
-          navigateToHelp();
-        }
-        else {
-          sessionStorage.setItem("showMessage", JSON.stringify(true));
-          window.location.reload();
-        }
-    })
-}
+    Axios.get(url).then((response) => {
+      // this will insert the data of the patient
+      let data = response.data[0];
 
-  const handleSubmit = async e => {
+      //If username is incorrect
+      if (data == null) {
+        sessionStorage.setItem("showMessage", JSON.stringify(true));
+        window.location.reload();
+      }
 
-      e.preventDefault();
-    
-      const token = await loginUser({
-    
-        username,
-    
-        password
-    
-      });
-    
-      setToken(token);
-    }
-  
-  const handleClick = event => {
-    LoadUsernameAndPassword("*", "Accounts", "Accounts", "username", username);
+      //If password is incorrect
+      if (password === data["password"]) {
+        sessionStorage.setItem("isLoggedIn", JSON.stringify(true));
+        sessionStorage.setItem("accountType", data["type"]);
+        sessionStorage.setItem("showMessage", JSON.stringify(false));
+        navigateToHelp();
+      } else {
+        sessionStorage.setItem("showMessage", JSON.stringify(true));
+        window.location.reload();
+      }
+    });
   }
 
-    return(
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
+    const token = await loginUser({
+      username,
+
+      password,
+    });
+
+    setToken(token);
+  };
+
+  const handleClick = (event) => {
+    LoadUsernameAndPassword("*", "Accounts", "Accounts", "username", username);
+  };
+
+  return (
     <>
-    <body className = "loginBody">
-      <div class="container-fluid text-center">
-          <div class="row content"> 
+      <body className="loginBody">
+        <div class="container-fluid text-center">
+          <div class="row content">
             <div class="col-sm-8 text-left">
-              <div className = "credentials">
-                <form className = "loginform">
-                  <div className = "CardPane">  
+              <div className="credentials">
+                <form className="loginform">
+                  <div className="CardPane">
                     <Card border="primary" className="LoginCard">
                       <Card.Header>Login to PIMS</Card.Header>
                       <Card.Body>
                         {incorrectUsernameOrPassword()}
-                        <label className = "username"><p>Username</p></label> {/*&nbsp adds a space*/}
-                        <input text = "text" required onChange={e => setUserName(e.target.value)}/>
-                        <br></br>   
-                        <label className = "password"><p>Password&nbsp;&nbsp;</p></label>{/*&nbsp&nbsp adds 2 spaces*/}
-                        <input type = "password" required onChange={e => setPassword(e.target.value)}/>
+                        <label className="username">
+                          <p>Username</p>
+                        </label>{" "}
+                        {/*&nbsp adds a space*/}
+                        <input
+                          text="text"
+                          required
+                          onChange={(e) => setUserName(e.target.value)}
+                        />
                         <br></br>
-                        <Button variant="outline-primary" className = "loginbutton" onClick={handleClick}>Login</Button>{' '}
+                        <label className="password">
+                          <p>Password&nbsp;&nbsp;</p>
+                        </label>
+                        {/*&nbsp&nbsp adds 2 spaces*/}
+                        <input
+                          type="password"
+                          required
+                          onChange={(e) => setPassword(e.target.value)}
+                        />
+                        <br></br>
+                        <Button
+                          variant="outline-primary"
+                          className="loginbutton"
+                          onClick={handleClick}
+                        >
+                          Login
+                        </Button>{" "}
                         {/*Add newlines to add 2 spaces above the button*/}
                         <br></br>
                         <br></br>
                         <Card.Title>Credentials</Card.Title>
                         <Card.Text>
-                          Please input your username and password given to you by your system administrator
-                        </Card.Text>    
+                          Please input your username and password given to you
+                          by your system administrator
+                        </Card.Text>
                       </Card.Body>
                     </Card>
                   </div>
-                </form> 
-              </div> 
+                </form>
+              </div>
             </div>
           </div>
         </div>
@@ -163,15 +165,11 @@ export default function Login({ setToken }) {
         <footer class="container-fluid text-center">
           <p>CS499 Team 3</p>
         </footer>
-        </body>
-    </>  
-  )
-
-
+      </body>
+    </>
+  );
 }
 
 Login.propTypes = {
-  
-  setToken: PropTypes.func.isRequired
-
+  setToken: PropTypes.func.isRequired,
 };
