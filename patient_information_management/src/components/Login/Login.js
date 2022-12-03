6561f7ecaf818cadfebf6afe5b77c0e74b52621c
benstone3 <111618446+benstone3@users.noreply.github.com>
2022-12-02 20:49:45 -0600

    cleanup

--- patient_information_management/src/components/Login/Login.js
+++ patient_information_management/src/components/Login/Login.js
@@ -1,40 +1,22 @@
-import React, { Component, useState } from 'react';
-import './Login.css';
+/*
+Authors: Timothy Hagler, Sydney Keller, Ben Stone, Laurel Strelzoff
+CS 499-01
+Senior Design Final Deliverable
+Professor Adam Colwell
+Fall 2022
+*/
 
+//import required libraries
+import React, { useState } from 'react';
+import './Login.css';
 import Button from 'react-bootstrap/cjs/Button.js';
 import Card from 'react-bootstrap/cjs/Card.js';
-import PropTypes from 'prop-types';
-import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import Axios from 'axios';
 import Alert from 'react-bootstrap/cjs/Alert.js';
 
-var accountType = sessionStorage.getItem("accountType")
 
-
-//async function required for login-authentication
-//to 8080 port where authentication server runs
-async function loginUser(credentials) {
-
-  return fetch('http://localhost:8080/login', {
- 
-    method: 'POST',
- 
-    headers: {
- 
-      'Content-Type': 'application/json'
- 
-    },
- 
-    body: JSON.stringify(credentials)
- 
-  })
- 
-    .then(data => data.json())
- 
- }
-
-export default function Login({ setToken }) {
-  
+export default function Login() {
   //Navigation functions
   const navigate = useNavigate();
 
@@ -54,27 +36,34 @@ export default function Login({ setToken }) {
     navigate('/about-us');
   }
 
+  //hooks used for username and password
   const [username, setUserName] = useState(); 
-  
   const [password, setPassword] = useState();
 
-  const [show, setShow] = useState(false); //Used for alert
+  //hook used for displaying the alert
+  const [setShow] = useState(); 
 
+
+  //function to display an alert of incorrect username or password
   function incorrectUsernameOrPassword()
   {
-    if(JSON.parse(sessionStorage.getItem("showMessage")))
+    //if the showMessage is true display the error
+    if (JSON.parse(sessionStorage.getItem("showMessage")))
+    {
       return(
         <>
           <div>
-            <Alert variant="danger" onClose={() => setShow(false)}>
+            {/*Alert (react-bootstrap component)*/}
+            <Alert variant="danger" onClose={() => setShow(false)}> 
               <Alert.Heading>Incorrect Username or Password</Alert.Heading>
             </Alert>
           </div>
         </>
       )
-      
+    }
   }
 
+  //function that loads the username and password from the 
   function LoadUsernameAndPassword(selection, schema, table, location, data) {
     let url = (`http://localhost:8080/api/getUsernameAndPassword/?selection=${selection}&schema=${schema}&table=${table}&location=${location}&data=${data}`)
     
@@ -82,49 +71,37 @@ export default function Login({ setToken }) {
         // this will insert the data of the patient
         let data = response.data[0]
 
-        //If username is incorrect
-        if (data == null)
+        //if username is incorrect
+        if (data == null) //data will not be present if username is incorrect
         {
-            sessionStorage.setItem("showMessage", JSON.stringify(true));
-            window.location.reload();
+            sessionStorage.setItem("showMessage", JSON.stringify(true)); // alert will be shown if username is incorrect stringify to store correctly
+            window.location.reload(); // reload the page to clear input fields
         }
         
-        //If password is incorrect
-        if(password === data["password"])
+        //if username AND password is correct
+        else if(password === data["password"])
         {
-          sessionStorage.setItem("isLoggedIn", JSON.stringify(true))
-          sessionStorage.setItem("accountType", data["type"])
-          sessionStorage.setItem("showMessage", JSON.stringify(false))
+          sessionStorage.setItem("isLoggedIn", JSON.stringify(true)) //set isLoggedIn sessionstorage object to true, need to stringify to store correctly
+          sessionStorage.setItem("accountType", data["type"]) //set account type in the sessionstorage for displaying different levels of clearance on patient list
+          sessionStorage.setItem("showMessage", JSON.stringify(false)) //alert will be set to false because correct credentials were input
           navigateToHelp();
         }
+
+        //if password is incorrect
         else {
           sessionStorage.setItem("showMessage", JSON.stringify(true));
           window.location.reload();
         }
     })
-}
+  }
 
-  const handleSubmit = async e => {
-
-      e.preventDefault();
-    
-      const token = await loginUser({
-    
-        username,
-    
-        password
-    
-      });
-    
-      setToken(token);
-    }
-  
+  //calls the function loadUsernameAndPassword when the login button is clicked
   const handleClick = event => {
     LoadUsernameAndPassword("*", "Accounts", "Accounts", "username", username);
   }
 
+    //render html the page
     return(
-
     <>
     <body className = "loginBody">
       <div class="container-fluid text-center">
@@ -136,7 +113,7 @@ export default function Login({ setToken }) {
                     <Card border="primary" className="LoginCard">
                       <Card.Header>Login to PIMS</Card.Header>
                       <Card.Body>
-                        {incorrectUsernameOrPassword()}
+                        {incorrectUsernameOrPassword()} {/*Show the alert if the username or password is incorrect*/}
                         <label className = "username"><p>Username</p></label> {/*&nbsp adds a space*/}
                         <input text = "text" required onChange={e => setUserName(e.target.value)}/>
                         <br></br>   
@@ -163,15 +140,7 @@ export default function Login({ setToken }) {
         <footer class="container-fluid text-center">
           <p>CS499 Team 3</p>
         </footer>
-        </body>
+      </body>
     </>  
   )
-
-
 }
-
-Login.propTypes = {
-  
-  setToken: PropTypes.func.isRequired
-
-};
