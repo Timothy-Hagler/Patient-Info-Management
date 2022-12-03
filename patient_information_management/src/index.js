0b86b651d4acb71660f23d2616d27e4ba363d974
benstone3 <111618446+benstone3@users.noreply.github.com>
2022-12-02 20:53:08 -0600

    cleanup

--- patient_information_management/src/index.js
+++ patient_information_management/src/index.js
@@ -1,3 +1,12 @@
+/*
+Authors: Timothy Hagler, Sydney Keller, Ben Stone, Laurel Strelzoff
+CS 499-01
+Senior Design Final Deliverable
+Professor Adam Colwell
+Fall 2022
+*/
+
+//import libraries
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
@@ -5,6 +14,7 @@ import App from './components/App/App.js';
 import reportWebVitals from './reportWebVitals.js';
 import "bootstrap/dist/css/bootstrap.min.css"
 
+//put everything created by react in DOM to be rendered in HTML
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
