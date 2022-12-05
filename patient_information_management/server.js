f3b702a0c7d13d419421d227fe590f6799c5bbc9
benstone3 <111618446+benstone3@users.noreply.github.com>
2022-12-04 19:57:51 -0600

    changed it from test (#39)

--- patient_information_management/server.js
+++ patient_information_management/server.js
@@ -16,7 +16,7 @@ app.use(cors());
 app.use(express.json())
 
 // Initializes connection using code in MySQL.js
-let connection = createConnection("test_user", "passwordtest", "anotherTest")
+let connection = createConnection("admindb", "password", "PIMS")
 connect(connection)
 
 app.listen(8080, () => console.log('API is running on http://localhost:8080/'));
